7a6815c7455bd9458d19eb78a45e419d10d824e4
Jan Matuszewski <luirun@gazeta.pl>
2017-08-14 22:09:28 +0200

    Poprawki html, poprawki w erb'ach, wrzucanie fotek do postow - nie sprawdzone

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  mount Ckeditor::Engine => '/ckeditor'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114181042) do
+ActiveRecord::Schema.define(version: 20170814220213) do
 
   create_table "ali_reviews", force: :cascade do |t|
     t.string   "productId",       limit: 45
@@ -33,6 +33,23 @@ ActiveRecord::Schema.define(version: 20161114181042) do
   end
 
   add_index "categories", ["id"], name: "id_UNIQUE", unique: true, using: :btree
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",    limit: 255, null: false
+    t.string   "data_content_type", limit: 255
+    t.integer  "data_file_size",    limit: 4
+    t.string   "data_fingerprint",  limit: 255
+    t.integer  "assetable_id",      limit: 4
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width",             limit: 4
+    t.integer  "height",            limit: 4
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.integer  "author",             limit: 4
@@ -81,7 +98,7 @@ ActiveRecord::Schema.define(version: 20161114181042) do
     t.text     "productUrl",           limit: 65535
     t.text     "promotionUrl",         limit: 65535
     t.text     "imageUrl",             limit: 65535
-    t.string   "originalPrice",        limit: 45
+    t.float    "originalPrice",        limit: 24
     t.float    "salePrice",            limit: 24
     t.text     "storeName",            limit: 65535
     t.text     "storeUrl",             limit: 65535
@@ -97,11 +114,19 @@ ActiveRecord::Schema.define(version: 20161114181042) do
     t.text     "aff_url",              limit: 65535
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
-    t.integer  "category",             limit: 4,     default: 50, null: false
+    t.integer  "category",             limit: 4,     default: 50
     t.integer  "subcategory",          limit: 4
     t.integer  "sub_subcategory",      limit: 4
     t.string   "is_hot",               limit: 3
     t.string   "is_approved",          limit: 3
+    t.string   "with_reviews",         limit: 1
+    t.string   "archived",             limit: 1
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "userId",       limit: 4
+    t.string  "userCookieId", limit: 45
+    t.string  "productId",    limit: 45
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -109,6 +134,8 @@ ActiveRecord::Schema.define(version: 20161114181042) do
     t.text     "short_description",  limit: 65535
     t.text     "long_description",   limit: 65535
     t.text     "keywords",           limit: 65535
+    t.float    "price",              limit: 24,    null: false
+    t.float    "rating",             limit: 24,    null: false
     t.text     "promoted",           limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -118,6 +145,7 @@ ActiveRecord::Schema.define(version: 20161114181042) do
     t.integer  "cover_file_size",    limit: 4
     t.datetime "cover_updated_at"
     t.integer  "author",             limit: 4
+    t.string   "productId",          limit: 45,    null: false
   end
 
   create_table "subcategories", force: :cascade do |t|
@@ -139,6 +167,7 @@ ActiveRecord::Schema.define(version: 20161114181042) do
     t.string   "avatar_content_type", limit: 255
     t.integer  "avatar_file_size",    limit: 4
     t.datetime "avatar_updated_at"
+    t.string   "is_admin",            limit: 3
   end
 
 end
